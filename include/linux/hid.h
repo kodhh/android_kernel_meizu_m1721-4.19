3fb5be25fedb6b360d8983865dd32534854b4bb7
root <root@runnervmg397c.bkzyw0wccrheno05bobeeaqtra.xx.internal.cloudapp.net>
2026-05-23 10:27:55 +0000

    arm64: dts: taco: replace pstore with ramoops reserved-memory
    - Replace old pstore_reserve_mem_region with compatible = "ramoops"
    - Move from 0x9ff00000 (1MB) to 0xb0000000 (8MB)
    - Add record/console/pmsg-size for proper pstore ramoops init

--- include/linux/hid.h
+++ include/linux/hid.h
@@ -676,9 +676,7 @@ struct hid_descriptor {
 	__u8  bCountryCode;
 	__u8  bNumDescriptors;
 
-	struct hid_class_descriptor rpt_desc;
-
-	struct hid_class_descriptor opt_descs[];
+	struct hid_class_descriptor desc[1];
 } __attribute__ ((packed));
 
 #define HID_DEVICE(b, g, ven, prod)					\
